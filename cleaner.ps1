cdf8818080a71c90461fa5f4d15f29ced0f70010
Jose Benhumea <joseramosb@gmail.com>
2023-04-22 23:11:25 -0600

    kleaner 3.0, backer 1.0, mixer 1.0. Se crea backer que realiza una copia de seguridad del directorio que le indiquemos hasta el momento kleaner como backer se les debe especificar la hubicacion del archivo que deseamos trabajar.Mixer es un archivo bat que utiliza cleaner y backer para crear la copia y borrarla.

---
+++ cleaner.ps1
@@ -0,0 +1,22 @@
+# Ruta del directorio que deseas eliminar
+$directoryPath = "C:\Users\jose.benhumea\Documents\Workbook 736"
+# Numero de veces que se sobrescribirá el contenido de los archivos antes de ser eliminados
+$numberOfOverwrites = 5
+# Genera el contenido aleatorio que se utilizará para sobrescribir los archivos
+$randomBytes = New-Object byte[] 1024
+$randomNumberGenerator = [System.Security.Cryptography.RNGCryptoServiceProvider]::Create()
+$randomNumberGenerator.GetBytes($randomBytes)
+# Recorre todos los archivos dentro del directorio y los sobrescribe varias veces con contenido aleatorio
+Get-ChildItem $directoryPath -Recurse | ForEach-Object {
+    if($_.GetType().Name -eq "FileInfo"){
+        for ($i = 0; $i -lt $numberOfOverwrites; $i++) {
+            $fileStream = New-Object System.IO.FileStream($_.FullName, [System.IO.FileMode]::Open)
+            $fileStream.Write($randomBytes, 0, $randomBytes.Length)
+            $fileStream.Dispose()
+        }
+    }
+}
+# Elimina el directorio y todo su contenido de forma permanente sin mostrar ningún mensaje en la consola
+Remove-Item $directoryPath -Force -Recurse -ErrorAction SilentlyContinue -Confirm:$false
+# Muestra un mensaje de finalizado
+Write-Host "El directorio $directoryPath ha sido eliminado de forma permanente."
